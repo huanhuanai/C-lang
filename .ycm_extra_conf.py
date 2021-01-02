f19e584a23784fa95a8c568a8b2c08c6986c86be
huanhuanai <695045212@qq.com>
2021-01-02 11:40:51 +0800

    .ycm_extra_conf

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,12 @@
+flags = [
+'-std=gunc99',
+'-x',
+'c',
+'-isystem',
+'/usr/include/',
+'-isystem',
+'/home/chunli/work/01_第三方共享库头文件路径/',
+'-isystem',
+'/home/chunli/work/01_chunli/08_本项目头文件路径/',
+
+]
